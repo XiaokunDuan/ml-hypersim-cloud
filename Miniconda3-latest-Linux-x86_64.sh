9245a4722f19198e378434e99d1f4ddbc6a34989
Xiaokun Duan <ylu665485@gmail.com>
2025-10-14 16:37:42 +0800

    Initial commit

---
+++ Miniconda3-latest-Linux-x86_64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dda3629462ba1cfa72eb74535214c2e315c77f1cfb0f02046537e99f1bf64abc
+size 162129736
---
+++ final_check.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:672378e447699f42cfab8e430a9c1f142b057352f892c6a07437e13bb952987f
+size 1860
